f77f6af31c14a63dc9d9911fad1d302f1c4bb8dc
viethaihust <vuviethaihust@gmail.com>
2023-11-15 21:23:59 +0700

    parameter

--- OOP/src/Lab02/DigitalVideoDisc.java
+++ OOP/src/Lab02/DigitalVideoDisc.java
@@ -10,6 +10,9 @@ public class DigitalVideoDisc {
 	public String getTitle() {
 		return title;
 	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 	public String getCategory() {
 		return category;
 	}
