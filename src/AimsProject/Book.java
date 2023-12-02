7402bf9255d63dbff1657b076d953f1c9482e946
viethaihust <vuviethaihust@gmail.com>
2023-12-02 17:24:11 +0700

    fix

---
+++ src/AimsProject/Book.java
@@ -0,0 +1,19 @@
+package AimsProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+
+	private int id;
+	private String title;
+	private String category;
+	private float cost;
+	private List<String> authors = new ArrayList<>();
+	
+	public Book() {
+		
+	}
+
+	
+}
